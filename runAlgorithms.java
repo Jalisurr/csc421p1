b1b89814db491e8b39c06de70ca233155c77c3ed
jostrat <stratton@uvic.ca>
2013-03-07 00:29:27 -0800

    Update runAlgorithms.java

--- runAlgorithms.java
+++ runAlgorithms.java
@@ -140,9 +140,73 @@ public class runAlgorithms {
 		}
 		// */
 			
+		// TESTING KERNELIZED PERCEPTRON
+		System.out.println("Testing the Kernelized Perceptron Algorithm:");
+		System.out.println("   -tested with 1, 2, 3, 4, 5, 10, 100, and 1000 iterations");
+		System.out.println("   -tested with 10/90, 25/75, and 40/60 splits of training/test data");
+		System.out.println();
+		System.out.println("Below are the most efficient cases with low test errors and a low number of iterations:");
+		System.out.println();
+	
+		// TEST CASE #1: The Pima Indian Diabetes Data Set
+		KernelPerceptron kp1 = new KernelPerceptron("pima-indians-diabetes.data", 3, 0.25);
+		kp1.convertClassifiers();									// Convert the classifiers to -1 and +1.
+		for (int i = 0; i < kp1.data.storage.length; i++)	// Normalize the data.
+		{
+			kp1.data.storage[i] = kp1.normalizeVector(kp1.data.storage[i]);
+		}
+		kp1.kernalPerceptron(2);		// Run kernelized perceptron for a maximum of 2 iterations.
+		
+		System.out.println("Case #1: The Pima Indian Diabetes Data Set");
+		System.out.println("     Degree: 3");
+		System.out.println("     Training Data: 25% - " + kp1.trainingSetSize + " instances");
+		System.out.println("     Test Data: 75% - " + kp1.testSetSize + " instances");
+		System.out.println("     Maximum Iterations: 2");
+		kp1.generateStatistics();		// Calculate and print the training and test errors.
+	
 		
 		
+		// TEST CASE #2: The Haberman Cancer Survival Rate
+		KernelPerceptron kp2 = new KernelPerceptron("haberman.data", 3, 0.10);
+		kp2.convertClassifiers();									// Convert the classifiers to -1 and +1.
+		for (int i = 0; i < kp2.data.storage.length; i++)	// Normalize the data.
+		{
+			kp2.data.storage[i] = kp2.normalizeVector(kp2.data.storage[i]);
+		}
+		kp2.kernalPerceptron(2);		// Run kernelized perceptron for a maximum of 2 iterations.
 		
+		System.out.println("Case #2: The Haberman Cancer Survival Rate");
+		System.out.println("     Degree: 3");
+		System.out.println("     Training Data: 10% - " + kp2.trainingSetSize + " instances");
+		System.out.println("     Test Data: 90% - " + kp2.testSetSize + " instances");
+		System.out.println("     Maximum Iterations: 2");
+		kp2.generateStatistics();		// Calculate and print the training and test errors.
+		
+		
+		// The section below can be uncommented and edited to run your own perceptron tests
+		
+/*
+		// These four variables can be modified as desired to run tests on perceptron.
+		String fileName = "pima-indians-diabetes.data";
+		int degree = 3;
+		double trainingPercentage = 0.25;
+		int maximumIterations = 10;
+		
+		KernelPerceptron kp3 = new KernelPerceptron(fileName, degree, trainingPercentage);
+		kp3.convertClassifiers();									// Convert the classifiers to -1 and +1.
+		for (int i = 0; i < kp3.data.storage.length; i++)	// Normalize the data.
+		{
+			kp3.data.storage[i] = kp3.normalizeVector(kp3.data.storage[i]);
+		}
+		kp3.kernalPerceptron(2);		// Run kernel perceptron.
+		
+		System.out.println("Running a customized kernelized perceptron test:");
+		System.out.println("     Degree: " + degree);
+		System.out.println("     Training Data: " + kp3.trainingSetSize + " instances");
+		System.out.println("     Test Data: " + kp3.testSetSize + " instances");
+		System.out.println("     Maximum Iterations: " + maximumIterations);
+		kp3.generateStatistics();		// Calculate and print the training and test errors.
+*/	
 	}
 	
 	//Create and run a  new BoostingAlgorithm for the passed data set 
@@ -193,4 +257,6 @@ public class runAlgorithms {
 		return 0;
 	}
 	 */
+	 
+	 
 }
