60244e66bf9f91ba729b2d16c2969f02c6108cc7
jostrat <stratton@uvic.ca>
2013-03-07 00:41:14 -0800

    Update DecisionStump.java

--- DecisionStump.java
+++ DecisionStump.java
@@ -3,7 +3,9 @@
  * 
  * DecisionStump.java
  * Last modified: February 18, 2013
- * Author:Torben Barsballe - V00696445
+ * Authors: Torben Barsballe - V00696445
+ * 	    Joanna Stratton - V00702087
+ * 	    David Williams - V00701616 
  * 
  * Classifies data points using the decision stumps method.
  */
